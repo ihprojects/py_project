e1a1111da7e3fc60da11d86f9060c08d5a7e95c3
ih <i_hagemann@gmx.de>
2021-01-22 22:04:28 +0100

    created main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+pygame.init()
+#create window
+screen = pygame.display.set_mode((800,500))
+
+run = True
+while run:
+    #pygame.time.delay(100) # This will delay the game the given amount of milliseconds. In our casee 0.1 seconds will be the delay
+    # 
+    for event in pygame.event.get():  # This will loop through a list of any keyboard or mouse events.
+        if event.type == pygame.QUIT: # Checks if the red button in the corner of the window is clicked
+            run = False  # Ends the game loop
+    
+    pygame.display.update()       #refresh window
+
+
+pygame.quit()  # If we exit the loop this will execute and close our game
+pygame.quit
